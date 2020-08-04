7f1e293062a2ffe654102151e9c1b3e228b49d5e
naomi_homma <hnm.n.1029@gmail.com>
2020-08-04 16:58:30 +0900

    Added a Pomodoro cycle function

--- src/containers/Countdown.jsx
+++ src/containers/Countdown.jsx
@@ -3,57 +3,90 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 import CountdownComponent from '../components/Countdown';
 
-const useCountdown = () => {
+const useCountdown = (limit) => {
+  const[workTime, setWorkTime] = useState(0.2);
+  const[breakTime, setBreakTime] = useState(0.3);
+  const[longBreakTime, setLongBreakTime] = useState(20);
+  const[timerState, setTimerState] = useState('work');
+  const[cycleCount, setCycleCount] = useState(1);
   const[active, setActive] = useState(false);
-  const[leftSec, setLeftSec] = useState();
+  let[leftSec, setLeftSec] = useState(workTime*60);
   let[timerObj, setTimerObj] = useState('');
-  const[elapsedTime, setElapsedTime] = useState(0);
-  const[startTime, setStartTime] = useState(0);
 
-
-  const countDown = (time) => {
-    let leftSec;
-    leftSec = (time * 60 *1000) - elapsedTime - (Date.now() - startTime);
-  };
-
-  const handleStart = (time) => {
-    if( !active ) {
-      clearTimeout(timerObj);
-      countDown(time);
-    }
+  const handleSwitch = () => {
+    //3回目のworkが終了したらlongbreakに入る
+    //longbreakまでの作業時間
+    if( timerState === 'work' && cycleCount === 3 ) {
+      setLeftSec(longBreakTime*60);
+      setTimerState('longBreak');
+      setCycleCount(1);
+    } else if (timerState === 'work') {
+      setLeftSec(breakTime*60);
+      setTimerState('break');
+      console.log('handleSwitchのwork');
+    } else if ( timerState === 'break' ) {
+      setLeftSec(workTime*60);
+      setTimerState('work');
+      setCycleCount(c => c + 1);
+      console.log(cycleCount); 
+    } 
   }
+
+  const handleStart = () => {
+    console.log('start click!')
+    setActive(true);
+  };
 
   const handleStop = () => {
     setActive(false);
     clearTimeout(timerObj);
-  }
+    console.log('stop click')
+  };
 
   const handleReset = () => {
-    setLeftSec(25);
-  }
+    switch (timerState) {
+      case "work": setLeftSec(workTime);
+        break;
+      case "break": setLeftSec(breakTime);
+        break;
+      case "longBreak": setLeftSec(longBreakTime);
+        break;
+    }
+  };
 
   useEffect(() => {
-    setTimerObj(setTimeout(() => {
-      setLeftSec(countDown());
-      }, 100)
-    );
+    if( active && leftSec > 0 ) {
+      setTimerObj(
+        setTimeout(() => {
+          setLeftSec(prev => prev - 1);
+        }, 1000)
+      )
+    } else if ( active && leftSec <= 0 ) {
+      clearTimeout(timerObj);
+      console.log(leftSec);
+      console.log(timerState);
+      handleSwitch();
+    } else {
+      clearTimeout(timerObj);
+    }
     return () => clearTimeout(timerObj);
-  },[]);
+  },[active, leftSec])
 
-  //デバッグ用
-  // console.log(remainigTime);
-  // console.log(isRunning);
-  // console.log(secLeft);
-  // console.log(elapsedTime);
+  return[[workTime, leftSec, active], [handleStart, handleStop, handleReset]];
+}
 
+
+const CountdownContainer = () => {
+  const[[workTime, leftSec, active], [handleStart, handleStop, handleReset]] = useCountdown(25*60);
   return (
     <CountdownComponent
       leftSec={leftSec}
-      hancleStart={handleStart}
+      active={active}
+      handleStart={handleStart}
       handleStop={handleStop}
       handleReset={handleReset}
     />
   );
 }
 
-export default useCountdown;
+export default CountdownContainer;
