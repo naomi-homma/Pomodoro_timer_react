075617f310f3701da5aab9bedf34af509e616aec
naomi_homma <hnm.n.1029@gmail.com>
2020-08-09 17:16:05 +0900

    Added a Header.jsx and reset.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset-advanced';
+import Header from './components/Header';
 import CountdownContainer from './containers/Countdown';
 import UserInputContainer from './containers/UserInput';
+import InputTimeDesplay from './components/InputTimeDesplay';
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
+
+//InputTimeDesplayはcomponentから直接(ロジック切り出していない)
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Pomodoro Timer</p>
-      </header>
+      <GlobalStyle />
+      <Header />
       <CountdownContainer />
       <UserInputContainer />
+      <InputTimeDesplay />
     </div>
   );
 }
