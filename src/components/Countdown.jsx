ce8c66f5812995bc98f80d550c326ce73cd2cf2d
naomi_homma <hnm.n.1029@gmail.com>
2020-08-16 15:25:58 +0900

    Edit UI with material UI and styled-component

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -2,28 +2,40 @@
 import React from 'react';
 import {secToMMSS} from '../lib/time';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import useCountdown from '../containers/Countdown';
+import styled from 'styled-components';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-      margin: theme.spacing(1),
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//       margin: theme.spacing(1),
+//   },
+// }));
 
+const StyledTimeDesplay = styled.div`
+  color: #282c34;
+  font-size: 100px;
+`;
+
+const StyledButton = styled(Button)`
+  &.MuiButton-root {
+    width: '130px';
+    font-size: 1.3rem;
+    margin-right: 10px;
+  }
+`;
 
 
 const CountdownComponent = ({leftSec, handleStart, handleStop, handleReset, active}) => {
-  const classes = useStyles();
+  // const classes = useStyles();
   // console.log(leftSec);
   // console.log(handleStart);
   return (
     <>
-      <div className="timerView_main">{secToMMSS(leftSec)}</div>
-      <div className={classes.root}>
-       <Button variant="contained" onClick={handleStart} disabled={active}>START</Button>
-       <Button variant="contained" onClick={handleStop} disabled={!active}>STOP</Button>
-       <Button variant="contained" onClick={handleReset} disabled={!active}>RESET</Button>
+      <StyledTimeDesplay>{secToMMSS(leftSec)}</StyledTimeDesplay>
+      <div className="module-spacer" />
+      <div>
+       <StyledButton variant="contained" onClick={handleStart} disabled={active}>START</StyledButton>
+       <StyledButton variant="contained" onClick={handleStop} disabled={active}>STOP</StyledButton>
+       <StyledButton variant="contained" onClick={handleReset} disabled={active}>RESET</StyledButton>
       </div>
     </>
   );
