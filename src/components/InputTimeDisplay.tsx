96fbda04c85b99ed4c53e67adc9765ed38731761
kenjimorita <kenjimoritata@gmail.com>
2020-09-20 08:55:42 +0900

    fix: nits

--- src/components/InputTimeDisplay.tsx
+++ src/components/InputTimeDisplay.tsx
@@ -5,7 +5,7 @@ import Table from '@material-ui/core/Table/index';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead/index';
+import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import styled from 'styled-components';
